ee3fd14a3ff42d96dbaf4a568bf38c0e013e2a7d
Dan Cervone <dcervone@fas.harvard.edu>
2015-11-15 04:10:32 -0500

    epv draw submissoin script

--- code/epv_draw.R
+++ code/epv_draw.R
@@ -20,7 +20,7 @@ def.micro <- microDefModel(new.dat)
 hyper <- getHyperParams(new.dat)
 ev.out <- evLineups(new.dat)
   
-fv.epv.list <- allCalcs(new.dat, hyper, def.micro, ev.out, nmic=5)
+fv.epv.list <- allCalcs(new.dat, hyper, def.micro, ev.out, nmic=50)
 save(fv.epv.list, file="fv.epv.list.Rdata")
 epv <- compressEPV(new.dat, fv.epv.list)
 
